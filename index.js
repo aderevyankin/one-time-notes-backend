15a162e4d4d1c462324c042670669a77791b78df
Andrey.Derevyankin <adderevyankin@gmail.com>
2023-08-09 15:12:50 +0300

    CORS politics

--- index.js
+++ index.js
@@ -9,6 +9,12 @@ const DB_URL = process.env.DB_URL;
 const app = express();
 
 app.use(express.json());
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    next();
+});
 app.use('/api', router);
 
 async function startApp() {
