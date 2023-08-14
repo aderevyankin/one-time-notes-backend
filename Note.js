905dab45f111f79a8f2ccc8f0e61359489dec3de
Andrey.Derevyankin <adderevyankin@gmail.com>
2023-08-14 16:53:27 +0300

    даты создания и прочтения + статус заметки (прочтена/не прочтена)

--- Note.js
+++ Note.js
@@ -1,8 +1,14 @@
 import mongoose from 'mongoose';
 
 const Note = new mongoose.Schema({
-    author: {type: String, required: true},
-    title: {type: String, required: false},
-    content: {type: String, required: true},
-});
+            author: {type: String, required: true},
+            title: {type: String, required: false},
+            content: {type: String, required: true},
+            isRead:{
+                type: Boolean,
+                default: false
+            }
+        },
+        {timestamps: true},
+);
 export default mongoose.model('Note', Note);
